c87c6fc4335d3337d3103b1f8bc08e867dad4cf5
Kiran Patil <kiranspatil686@gmail.com>
2025-04-03 18:15:23 +0530

    update news api key add into the env file

--- src/app/NewsSection/page.js
+++ src/app/NewsSection/page.js
@@ -12,11 +12,12 @@ export default function NewsSection() {
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        const api_key = process.env.NEXT_PUBLIC_NEWS_API_KEY; // Use the env variable
         const response = await fetch(
-          `https://newsdata.io/api/1/news?apikey=pub_779173c98e52c288f16694032f52cd7349ae8&q=cryptocurrency&language=en`
+          `https://newsdata.io/api/1/news?apikey=${api_key}&q=cryptocurrency&language=en`
         );
         const data = await response.json();
-        console.log("Top news ",data);
+        console.log("Top news ", data);
         setNews(data.results.slice(0, 5));
         setLoading(false);
       } catch (err) {
@@ -37,17 +38,22 @@ export default function NewsSection() {
         ← Back
       </button>
 
-      <h1 className="text-3xl font-bold text-yellow-400 text-center">Latest Crypto News</h1>
-      <p className="text-gray-400 text-center mb-6">Stay updated with the latest cryptocurrency trends.</p>
+      <h1 className="text-3xl font-bold text-yellow-400 text-center">
+        Latest Crypto News
+      </h1>
+      <p className="text-gray-400 text-center mb-6">
+        Stay updated with the latest cryptocurrency trends.
+      </p>
 
       {loading && <p className="text-gray-300 text-center">Loading news...</p>}
       {error && <p className="text-red-500 text-center">{error}</p>}
 
-  
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {news.map((article, index) => (
-          <div key={index} className="bg-gray-800 p-4 rounded-lg shadow-md hover:shadow-xl transition duration-300">
-           
+          <div
+            key={index}
+            className="bg-gray-800 p-4 rounded-lg shadow-md hover:shadow-xl transition duration-300"
+          >
             {article.image_url ? (
               <img
                 src={article.image_url}
@@ -60,16 +66,15 @@ export default function NewsSection() {
               </div>
             )}
 
-         
             <h2 className="text-lg font-semibold text-white hover:text-yellow-400 transition duration-200">
               <a href={article.link} target="_blank" rel="noopener noreferrer">
                 {article.title}
               </a>
             </h2>
 
-           
             <p className="text-gray-400 text-sm mt-2">
-              {article.source_id} • {new Date(article.pubDate).toLocaleDateString()}
+              {article.source_id} •{" "}
+              {new Date(article.pubDate).toLocaleDateString()}
             </p>
           </div>
         ))}
